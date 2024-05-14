6e2984c226c93f82f251d8077e3298cb55dd0474
xilin <xilin@10.150.65.70>
2024-05-14 19:12:10 -0300

    feat: atribuição das classes

--- src/main/java/model/Amigo.java
+++ src/main/java/model/Amigo.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package model;
+package projeto.aluguel.ferramenta.Model;
 
-/**
- *
- * @author 1072415701
- */
 public class Amigo {
+
+    private int id;
+    private String nome;
+    private int telefone;
+    private int ferramentasPegas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
     
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
 }
